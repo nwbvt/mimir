26f791ed7c95e985c20fce9469c28d1afe69e5ab
Nicholas Brown <nwbrown@gmail.com>
2025-02-25 18:16:36 -0500

    Add training loop

--- mimir/training.py
+++ mimir/training.py
@@ -1,17 +1,22 @@
+import torch
+from torch.utils.data import DataLoader
 
 class Trainer(object):
 
-    def __init__(self, data, model, loss_fn, optimizer, device="cpu", seed=0, train_size=0.9):
+    def __init__(self, data, model, loss_fn, optimizer, device="cpu", seed=0, train_size=0.9, metrics={}, batch_size=128):
         generator = None
         if seed:
             generator = torch.Generator().manual_seed(seed)
             torch.manual_seed(seed)
-        self.train_data, self.test_data = torch.utils.data.random_split(data, [train_size, 1-train_size],
-                                                                        generator=generator)
-        self.model = model
+        train_data, test_data = torch.utils.data.random_split(data, [train_size, 1-train_size],
+                                                              generator=generator)
+        self.train_data = DataLoader(train_data, batch_size=batch_size)
+        self.test_data = DataLoader(test_data, batch_size=batch_size)
+        self.model = model.to(device)
         self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.device = device
+        self.metrics = metrics
 
     def eval(self, x, y):
         x = x.to(self.device)
@@ -30,20 +35,53 @@ class Trainer(object):
             if batch % 100 == 0 and log:
                 print(f"loss: {loss:>7f} [{batch * len(x):>6d}/{size:>6d}]", end="\r")
 
-    def test(self, train_data=False, metrics=[]):
+    def test(self, train_data=False):
         data = self.train_data if train_data else self.test_data
         size = len(data.dataset)
         num_batches = len(data)
         self.model.eval()
         loss = 0
-        metric_results = [0 for _ in metrics]
+        metric_results = {name: 0 for name in self.metrics}
         with torch.no_grad():
             for x, y in data:
                 pred = self.eval(x, y)
                 loss = self.loss_fn(pred, y.to(self.device))
-                for i, metric in enumerate(metrics):
-                    metric_results[i] += metric(x, pred)
+                if self.metrics:
+                    for name, metric in self.metrics.items():
+                        metric_results[name] += metric(x, pred)
         loss /= num_batches
         metric_results = [result/num_batches for result in metric_results]
         return loss, metric_results
-        
+
+    def train(self, max_epochs, max_streak=10, log=True):
+        best_params = None
+        best_loss = None
+        streak_len = 0
+        for i in range(0, max_epochs):
+            if log:
+                print(f"Epoch {i}")
+            self.train_epoch(log)
+            train_loss, train_metrics = self.test(True)
+            if log:
+                print(f"  Train: Loss={train_loss}", end="")
+                if train_metrics:
+                    for name, metric in train_metrics.items():
+                        print(f" {name}={metric}", end="")
+                print("")
+            test_loss, test_metrics = self.test(False)
+            if log:
+                print(f"  Test:  Loss={test_loss}", end="")
+                if test_metrics:
+                    for name, metric in test_metrics.items():
+                        print(f" {name}={metric}", end="")
+                print("")
+            if (best_loss is None) or (test_loss < best_loss):
+                streak_len = 0
+                best_loss = test_loss
+                best_params = self.model.state_dict()
+            else:
+                streak_len += 1
+                if streak_len >= max_streak:
+                    break
+        self.model.load_state_dict(best_params)
+        return self.model
